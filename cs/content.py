d30d40593c98fd02041a6cecbc1e25ebd6a0a200
Brandon <grigullbrandon@gmail.com>
2018-08-23 17:24:00 -0700

    add tagging support

--- cs/content.py
+++ cs/content.py
@@ -1,14 +1,19 @@
 import logging
+import re
 
-class Content(object):
+class Content:
+    RAW_TEXT_IDENTIFIER = "RAW TEXT"
+
     def __init__(self, raw_text):
         self.raw_text = raw_text
+        self.tag_char = "\#"
 
     def __str__(self):
-        return "RAW: " + self.raw_text
+        return RAW_TEXT_IDENTIFIER + self.raw_text
 
-    def __repr__(self):
-        return self.__str__()
+    @property
+    def tag_regex(self):
+        return '(?<={})'.format(self.tag_char)
 
     def encode_text(self, encoding):
         try:
@@ -21,6 +26,21 @@ class Content(object):
             self.raw_text = self.raw_text.decode(encoding)
         except LookupError:
             logging.warning('Cannot decode. Encoding %s is not valid.' % encoding)
+
+    def has_tags(self):
+        return re.search(self.tag_regex, self.raw_text) is not None
+
+    def parse_tags():
+        tag_matches = re.search(self.tag_regex, self.raw_text)
+        tags, index = [], 0
+        while True:
+            try:
+                tags.append(tag_matches.group(index))
+            except IndexError:
+                break
+
+        return tags
+
 
 
 
